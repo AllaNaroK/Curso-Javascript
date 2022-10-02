faffd9bf5075e2a44d0a26ab3c47ab116976c806
Allan Narok <allanarok@gmail.com>
2022-10-01 22:34:09 -0300

    Adding some descriptions to the exercices

--- Exercicio2.js
+++ Exercicio2.js
@@ -8,3 +8,8 @@ function retornarMaiorValor(a,b){
         return b;}
 
 console.log(maiorValor);
+
+//Precisamos comparar dois valores para mostrarmos o maior entre eles.
+//Passo 1: Criar uma variável para receber os valores.
+//Passo 2: Criar a função para comparar os valores.
+//Passo 3: Exibir o restultado (da variável criada) no console.
