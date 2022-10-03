d61d91931e692ba61db0b1cf59e9dc72ae9c520c
Allan Narok <allanarok@gmail.com>
2022-10-03 05:09:35 -0300

    Adding the ninth exercise with comments

---
+++ Exercicio9.js
@@ -0,0 +1,11 @@
+exibirAsteriscos(10);
+
+function exibirAsteriscos(linhas) {
+    let a = '';
+    for (let linha = 0; linha <= linhas; linha++){
+            console.log(a += '*');}}
+
+// Devemos criar um algoritmo para mostrar x vezes na tela uma string (da qual terá um acrécimo crescente)
+// Passo 1: Criar uma variável para armazenar a quantidade de vezes que a string será exibida na tela
+// Passo 2: Criar uma função com um laço para calcular a quantidade de linhas
+// Passo 3: Implementar ao algoritmo para mostrar na tela
