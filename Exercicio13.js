edee94410b10331d3100ba6be9bb6857bd7e327d
Allan Narok <allanarok@gmail.com>
2022-10-03 06:07:44 -0300

    Adding the thirteenth exercise with comments

---
+++ Exercicio13.js
@@ -0,0 +1,16 @@
+let postagem = {
+    titulo: 'Ordem Paranormal - RPG',
+    mensagem: 'Logo estará de volta!',
+    autor: 'Allan Narok',
+    visualizacoes: 1044,
+    comentarios: [
+        { autor: 'Daniel K.', mensagem: 'Logo mesmo...'},
+        { autor: 'R.L.', mensagem: 'Outubro .-.'}],
+    aoVivo: true} 
+    console.log(postagem);
+
+
+// Devemos criar um algoritmo que simule uma postagem no estilo "blog".
+// Passo 1: Criar um objeto para armazenar todas as informações solicitadas
+// Passo 2: Criar um array (comentários)
+// Passo 3: Mostrar todas as informações na tela
